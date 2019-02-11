8cce48a0ecb110bd90a1268d5497dc3c039298f7
dandeeccastro <dundeepessoal@gmail.com>
2019-02-11 16:37:20 -0200

    *lógica básica de display-jogos no ts

--- indie-invaders/src/app/pages/home/display-jogos/display-jogos.component.ts
+++ indie-invaders/src/app/pages/home/display-jogos/display-jogos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-display-jogos',
@@ -7,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayJogosComponent implements OnInit {
 
-  constructor() { }
+	@Input() index: number;
+	private titulos = [
+		"Popular", "Promoções", "Para Você"	
+	];
 
-  ngOnInit() {
- 	 
-  }
+	constructor() { 
+	}
+
+  	ngOnInit() { 
+ 		console.log(this.index);
+  	}
 
 }
