114ca93348a6f7999be909f5303dd6ca75944fe5
Igor <iminichettibuto@gmail.com>
2019-02-19 15:03:36 -0300

    # Cadastro sem component na page cadastro

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'indie-invaders';
+  
+  data = {
+    password: '',
+    password_confirm: '',
+  };
 }
--- Angular/src/app/pages/cadastro/cadastro.component.ts
+++ Angular/src/app/pages/cadastro/cadastro.component.ts
@@ -18,4 +18,47 @@ export class CadastroComponent implements OnInit {
   onSubmit(cadastro){
     console.log(cadastro);
   }
+
+  validateCPF(cpf) {
+    let cpfArray = cpf.value.toString();
+    let falsePositive: boolean = true;
+    // CPF tem 11 digitos?
+    console.log(cpfArray.length);
+    if (cpfArray.length == 11){
+      // CPF é um falso positivo?
+      for (let digit of cpfArray) {
+        if (digit != cpfArray[0]) { falsePositive = false; }
+      }
+      console.log(falsePositive);
+      if(!falsePositive){
+        // Cálculo de validação do CPF
+        let i = 10; let j = i + 1;
+        let firstVerifier = 0;
+        let secondVerifier = 0;
+        for (let digit of cpfArray){
+          if (i != 1) { 
+            firstVerifier += i * digit;
+            i--;
+          }
+          if (j != 1) {
+            secondVerifier += j * digit;
+            j--;
+          }
+        }
+        firstVerifier = (firstVerifier * 10) % 11;
+        secondVerifier = (secondVerifier * 10) % 11;
+        console.log(firstVerifier, secondVerifier);
+        if (firstVerifier == cpfArray[9] && secondVerifier == cpfArray[10]){
+          this.cpfIsValid = true;
+        }
+      } else {
+        this.cpfIsValid = false;
+      }
+      
+    } else {
+      this.cpfIsValid = false;
+    }
+    console.log(this.cpfIsValid);
+    return this.cpfIsValid;
+  }
 }
